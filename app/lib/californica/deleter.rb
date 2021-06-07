0ba2e526f303f3cef19a53ad2dad903da4db48b5
Andrew Wallace <andrewwallace@library.ucla.edu>
2021-06-07 14:15:28 -0700

    Fix bug in Californica::Deleter logging

--- app/lib/californica/deleter.rb
+++ app/lib/californica/deleter.rb
@@ -41,13 +41,11 @@ module Californica
         record_name = "#{record.class} #{record.ark}"
         record&.destroy&.eradicate
         Hyrax.config.callback.run(:after_destroy, record.id, User.batch_user)
-        logger.info("Deleted #{record_name || id}}")
       end
 
       def delete_from_fcrepo
         ActiveFedora.fedora.connection.delete(ActiveFedora::Base.id_to_uri(id))
         Rollbar.info("Forced delete of #{id} from Fedora")
-        logger.info("Forced delete of #{id} from Fedora")
       rescue Ldp::NotFound
         nil # Everything's good, we just wanted to make sure there wasn't a record in fedora not indexed to solr
       end
